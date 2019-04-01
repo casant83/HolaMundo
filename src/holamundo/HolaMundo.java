20350dec2138069f35a461389e22ecfacba6eba4
Carmen Santana Enríquez <csantanaenriquez@gmail.com>
2019-04-01 12:01:45 +0200

    Código fuente inicial

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,15 @@
+
+package holamundo;
+
+/**
+ *
+ * @author Carmen Santana
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Ejemplo para pruebas");
+    }
+    
+}
